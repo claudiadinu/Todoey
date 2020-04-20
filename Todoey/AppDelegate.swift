090f4f161f6ffead94374ee58a7b7c12090113a1
Claudia Dinu <Claudia.Dinu@ibm.com>
2020-04-20 18:23:56 +0300

    Persisting Data using Core Data

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,11 +7,10 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("did finish launching with options")
@@ -24,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+/*    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
@@ -35,8 +34,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+*/
     
-    func applicationWillResignActive(_ application: UIApplication) {
+/*  func applicationWillResignActive(_ application: UIApplication) {
         
     }
     
@@ -51,9 +51,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         
     }
+*/
     
     func applicationWillTerminate(_ application: UIApplication) {
         print("application will terminate")
+        self.saveContext()
+    }
+
+    
+    
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
 
--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -7,40 +7,31 @@
 //
 
 import UIKit
+import CoreData
 
-class ToDoListViewController: UITableViewController {
+class ToDoListViewController: UITableViewController{
     
-//    var itemArray = ["Find Mike", "Buy Eggs", "Play Drawful", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"]
     var itemArray = [Item]()
+    
+    //optional variable - bc it's going to be nil until we set it in CategoryVC
+    var selectedCategory : Category? {
+        //this happens as soon as the selectedCategory var is set with a value
+        didSet {
+            loadItems()
+        }
+    }
     
     let defaults = UserDefaults.standard
     
-    //path to the document directory
-    //we'll create a new plist at the location of our document file path, instead of using UserDefaults
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //we need an intance of AppDelegate tp access its properties
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
-        
-//        let newItem = Item()
-//        newItem.title = "Find Mike"
-//        itemArray.append(newItem)
-//        let newItem2 = Item()
-//        newItem2.title = "Buy Eggs"
-//        itemArray.append(newItem2)
-//        let newItem3 = Item()
-//        newItem3.title = "Play Drawful"
-//        itemArray.append(newItem3)
-        
-        //previously we were retrieving an array of Strings
-        //retrieve data from itemArray
-//        if let items = UserDefaults.standard.array(forKey: "ToDoListArray") as? [Item] {
-//            itemArray = items
-//        }
-        
-        loadItems() //from Items.plist
+        //file path to view core data -> Library -> Application Support
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+
     }
     
     //MARK: - TableView DataSource Methods
@@ -52,13 +43,6 @@ class ToDoListViewController: UITableViewController {
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
         
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        }
-//        else{
-//            cell.accessoryType = .none
-//        }
-        //ternary operator
         cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
@@ -73,26 +57,17 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
         
-//        if itemArray[indexPath.row].done == false {
-//            itemArray[indexPath.row].done = true
-//        }
-//        else{
-//            itemArray[indexPath.row].done = false
-//        }
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //update the item - setValue function
+        //itemArray[indexPath.row].setValue("Completed", forKey: "title")
+        
+        //delete item
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         //sets the done attribute to the opposite - reversing what it used to be
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItems()
-        
-        //tableView.reloadData()  // forces the table view to call its data source methods again
-        
-        //add/remove checkmark whenever a cell is selected
-//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        }
-//        else{
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
         
         //after the user selects the row, it becomes gray, but after a second it's deselected and becomes white
         tableView.deselectRow(at: indexPath, animated: true)
@@ -106,16 +81,13 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user click the Add button on our UIalert
-            //print(textField.text)
-            //we can prevent the execution of this line of code if textField is empty
-            //self.itemArray.append(textField.text!)
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            //we need to set a value for done in order to have the item saved (and not have a nil valuefor done property)
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
-            //store the array in the user defaults with a given key
-            //this line fails when setting an araay of Items -> crash
-            //self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.saveItems()
         }
         alert.addTextField { (alertTextField) in
@@ -129,34 +101,69 @@ class ToDoListViewController: UITableViewController {
     
     //MARK: - Model Manipulation Methods
     
+    //using core data for saving our new items
     func saveItems(){
-        let encoder = PropertyListEncoder()
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            //save that sketch, data found in temporary area becomes permanent
+            try context.save()
         }
         catch {
-            print("Error encoding itemArray, \(error)")
+            print("Error saving context, \(error)")
         }
         
         //item was added in the array, but the table view needs to be changed
-        self.tableView.reloadData()
+        self.tableView.reloadData()  // forces the table view to call its data source methods again
     }
     
-    func loadItems(){
-        //using optional binding to unwrap that safely
-        //added try? because it's trowing
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            //decode the data from the file path
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-            }
-            catch{
-                print("Error decoding item array, \(error)")
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil){
+        //the entity you are trying to request data from is mandatory
+        //we provide a default value to be used in viewDidLoad()
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        }
+        else {
+            request.predicate = categoryPredicate
+        }
+        
+        do{
+            itemArray = try context.fetch(request)
+        }
+        catch{
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+
+}
+
+//MARK: - Search bar methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        //for all the items in the itemArray look for the ones where the title contains this text
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        //request.predicate = predicate
+        
+        //items sorted asc by title
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                //cursor and keyboard should go away - on the main thread
+                searchBar.resignFirstResponder()
             }
         }
     }
-
+    
 }
 
